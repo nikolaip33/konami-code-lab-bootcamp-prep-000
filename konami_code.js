ef21fddf281d4abac57d1e54efed4062a3bfe04c
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-22 03:08:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,6 +5,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
+  console.log("something")
   document.body.addEventListener('keypress', function(e) {
     console.log("something")
     var key = parseInt(e.detail || e.which);
