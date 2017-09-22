5bc3247df313c0e4bad242fe1018cdbeed25b3b6
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-22 02:49:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,25 @@
 console.log("Let the games begin????")
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-let index = 0;
+
 
 function init() {
-const body = document.body;
-body.addEventListener('keypress', function(e) {
-  var key = parseInt(e.detail || e.which);
-    if(key === code[index]){
-      index++;
-      console.log("test")
-      if(index === code.length){
-      alert("Congratulations!!");
-      index=0;
+  let index = 0;
+  const body = document.body;
+  body.addEventListener('keypress', function(e) {
+    var key = parseInt(e.detail || e.which);
+      if(key === code[index]){
+        index++;
+        console.log("test")
+        if(index === code.length){
+          alert("Congratulations!!");
+          index=0;
+        }
+        else {
+          index = 0;
+        }
       }
-  else {
-    index = 0;
-  }
-}
-});
+    }
+  });
 }
 init()
