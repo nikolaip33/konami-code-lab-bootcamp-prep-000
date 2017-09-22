f5e804f183101bace9cf662f93af9343eeb1cbce
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-22 03:20:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,11 @@ console.log("Let the games begin????")
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 
+
+
 function init() {
 let index = 0
-  document.body.addEventListener('keypress', function(e) {
+  document.body.addEventListener('keydown', function(e) {
     console.log("test")
     const key = parseInt(e.detail || e.which);
     if(key === code[index]){
